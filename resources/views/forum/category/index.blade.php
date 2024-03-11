d211669d27cd4622a0a84e9fae97edd528167a5b
Eli <elizer.engana.jr@gmail.com>
2024-03-11 22:47:53 +0800

    Motomart Forum v1

--- resources/views/forum/category/index.blade.php
+++ resources/views/forum/category/index.blade.php
@@ -3,7 +3,7 @@
 
 @section ('content')
     <div class="flex flex-row justify-between my-3">
-        <h2 class="grow text-3xl font-medium">{{ trans('forum::general.index') }}</h2>
+        <h2 class="grow text-3xl font-medium">Home</h2>
 
         @can ('createCategories')
             <x-forum.button type="button" data-open-modal="create-category">
